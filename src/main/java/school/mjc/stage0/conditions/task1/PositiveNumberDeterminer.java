08cb9dae93aed88dee9fdc2eef18b124fd6971b3
Axrich <116589250+Axrich@users.noreply.github.com>
2023-07-21 17:15:44 +0500

    Update PositiveNumberDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -1,7 +1,5 @@
 package school.mjc.stage0.conditions.task1;
 
-package school.mjc.stage0.conditions.task1;
-
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
         if (numberToBeDetermined > 0) {
